00df46000166447dd2a40b4878cd410d08bff04f
michael <insominx@yahoo.com>
2024-09-13 14:04:35 -0700

    Code cleanup and updated meta scene to correctly launch the wireframe example.

--- Assets/Custom/MeshTexturedWireframeAdapter.cs
+++ Assets/Custom/MeshTexturedWireframeAdapter.cs
@@ -9,36 +9,31 @@
 // %BANNER_END%
 using System.Collections.Generic;
 using UnityEngine;
-using System.Linq;
-using UnityEngine.XR.ARFoundation;
-using UnityEngine.XR.OpenXR;
-using UnityEngine.XR.ARSubsystems;
-using UnityEngine.XR;
-
 /// <summary>
-/// Adapts and prepares meshes from ARMeshManager to use the TexturedWireframe material and shader.
-/// <para>Somewhat based on the texture-based wireframe technique described in
-/// http://sibgrapi.sid.inpe.br/col/sid.inpe.br/sibgrapi/2010/09.15.18.18/doc/texture-based_wireframe_rendering.pdf
-/// See Figure 5c and related description</para>
-///
-/// This version was adapted from the one provided by Magic Leap
+///     Adapts and prepares meshes from ARMeshManager to use the TexturedWireframe material and shader.
+///     <para>
+///         Somewhat based on the texture-based wireframe technique described in
+///         http://sibgrapi.sid.inpe.br/col/sid.inpe.br/sibgrapi/2010/09.15.18.18/doc/texture-based_wireframe_rendering.pdf
+///         See Figure 5c and related description
+///     </para>
+///     This version was adapted from the one provided by Magic Leap
 /// </summary>
 public class MeshTexturedWireframeAdapter : MonoBehaviour
 {
-    [SerializeField, Tooltip("The textured wireframe material.")]
-    private Material wireframeMaterial = null;
+    [SerializeField][Tooltip("The textured wireframe material.")]
+    Material wireframeMaterial;
 
-    private Texture2D proceduralTexture = null;
-    private int lineTextureWidth = 2048;       // Overall width of texture used for the line (will be 1px high)
-    private int linePixelWidth = 24;           // Line fill pixel width (left side) representing line, over background
-    private int lineEdgeGradientWidth = 4;     // Falloff gradient pixel size to smooth line edge
+    readonly float[] confidenceData = new float[0];
+    readonly List<int> indices = new();
+    readonly int lineEdgeGradientWidth = 4; // Falloff gradient pixel size to smooth line edge
+    readonly int linePixelWidth = 24; // Line fill pixel width (left side) representing line, over background
+    readonly int lineTextureWidth = 2048; // Overall width of texture used for the line (will be 1px high)
 
-    float[] confidenceData = new float[0];
-    private List<Vector3> vertices = new List<Vector3>();
-    private List<int> indices = new List<int>();
-    private List<Vector3> uvs = new List<Vector3>();
+    Mesh meshReference;
 
-    private Mesh meshReference;
+    Texture2D proceduralTexture;
+    readonly List<Vector3> uvs = new();
+    readonly List<Vector3> vertices = new();
 
     public bool ComputeConfidences { get; set; }
 
@@ -50,12 +45,12 @@ public class MeshTexturedWireframeAdapter : MonoBehaviour
         {
             // Create procedural texture used to render the line (more control this way over mip-map levels)
             proceduralTexture = new Texture2D(lineTextureWidth, 1, TextureFormat.ARGB32, 7, true);
-            int w = linePixelWidth - (lineEdgeGradientWidth / 2);
+            int w = linePixelWidth - lineEdgeGradientWidth / 2;
             for (int i = 0; i < lineTextureWidth; i++)
             {
-                var color = i <= w ? Color.white :
-                            i > w + lineEdgeGradientWidth ? Color.clear :
-                            Color.Lerp(Color.white, Color.clear, (i - w) / (float)lineEdgeGradientWidth);
+                Color color = i <= w ? Color.white :
+                    i > w + lineEdgeGradientWidth ? Color.clear :
+                    Color.Lerp(Color.white, Color.clear, (i - w) / (float)lineEdgeGradientWidth);
                 proceduralTexture.SetPixel(i, 0, color);
             }
             proceduralTexture.wrapMode = TextureWrapMode.Clamp;
@@ -63,7 +58,7 @@ public class MeshTexturedWireframeAdapter : MonoBehaviour
 
             wireframeMaterial.mainTexture = proceduralTexture;
             OnMeshUpdatedOrAdded();
-        } 
+        }
     }
 
     void OnDestroy()
@@ -75,13 +70,13 @@ public class MeshTexturedWireframeAdapter : MonoBehaviour
         }
     }
 
-    private void OnMeshUpdatedOrAdded()
+    void OnMeshUpdatedOrAdded()
     {
         MeshFilter filter = GetComponent<MeshFilter>();
         ConfigureTrianglesForMesh(filter);
     }
 
-    private void ConfigureTrianglesForMesh(MeshFilter meshFilter)
+    void ConfigureTrianglesForMesh(MeshFilter meshFilter)
     {
         // Adapt the mesh for the textured wireframe shader.
         if (meshFilter != null)
@@ -101,7 +96,7 @@ public class MeshTexturedWireframeAdapter : MonoBehaviour
             // Encode confidence in uv.z
             for (int i = 0; i < uvs.Count; i++)
             {
-                var uv = uvs[i];
+                Vector3 uv = uvs[i];
                 uv.z = validConfidences ? confidenceData[i] : 1;
                 uvs[i] = uv;
             }
@@ -109,42 +104,42 @@ public class MeshTexturedWireframeAdapter : MonoBehaviour
             int indicesOrigCount = indices.Count;
             for (int i = 0; i < indicesOrigCount; i += 3)
             {
-                var i1 = indices[i];
-                var i2 = indices[i + 1];
-                var i3 = indices[i + 2];
+                int i1 = indices[i];
+                int i2 = indices[i + 1];
+                int i3 = indices[i + 2];
 
-                var v1 = vertices[i1];
-                var v2 = vertices[i2];
-                var v3 = vertices[i3];
+                Vector3 v1 = vertices[i1];
+                Vector3 v2 = vertices[i2];
+                Vector3 v3 = vertices[i3];
 
-                var uv1 = uvs[i1];
-                var uv2 = uvs[i2];
-                var uv3 = uvs[i3];
+                Vector3 uv1 = uvs[i1];
+                Vector3 uv2 = uvs[i2];
+                Vector3 uv3 = uvs[i3];
 
                 // Create a new center vertex of each triangle, adjusting indices and add new triangles
                 // Will use Incenter of Triangle (center that is equidistant to edges).
                 // This allows the line width to be consistent regardless of triangle size.
                 // Also allows line width to be adjusted dynamically.
                 // Calculate position of incenter vertex
-                var a = Vector3.Distance(v2, v3);
-                var b = Vector3.Distance(v1, v3);
-                var c = Vector3.Distance(v1, v2);
-                var sum = a + b + c;
-                var vIntercenter = new Vector3((a * v1.x + b * v2.x + c * v3.x) / sum,
-                                               (a * v1.y + b * v2.y + c * v3.y) / sum,
-                                               (a * v1.z + b * v2.z + c * v3.z) / sum);
+                float a = Vector3.Distance(v2, v3);
+                float b = Vector3.Distance(v1, v3);
+                float c = Vector3.Distance(v1, v2);
+                float sum = a + b + c;
+                Vector3 vIntercenter = new Vector3((a * v1.x + b * v2.x + c * v3.x) / sum,
+                    (a * v1.y + b * v2.y + c * v3.y) / sum,
+                    (a * v1.z + b * v2.z + c * v3.z) / sum);
                 vertices.Add(vIntercenter);
                 int iC = vertices.Count - 1;
 
                 // Distance to edge, or radius of incircle
-                var s = sum / 2.0f;
-                var r = Mathf.Sqrt(((s - a) * (s - b) * (s - c)) / s);
+                float s = sum / 2.0f;
+                float r = Mathf.Sqrt((s - a) * (s - b) * (s - c) / s);
 
                 // Calculate UV for the incenter vertex for a 1mm target line width
                 // Half of each line is rendered on the edges of each triangle, so .001/2 = .0005
                 // Can be adjusted in shader to vary line width dynamically.
                 float lineWidth = .0005f;
-                float segmentPixels = (r / lineWidth) * linePixelWidth;
+                float segmentPixels = r / lineWidth * linePixelWidth;
                 float segmentUV = segmentPixels / lineTextureWidth;
 
                 Vector3 centerUV = Vector3.one * segmentUV;
